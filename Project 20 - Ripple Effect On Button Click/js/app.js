360863a1934b689729b94ce9c9d1f2eb655bf57a
mustafa-kapadia1483 <mustafakapadia76@gmail.com>
2021-12-04 21:47:01 +0530

    Day 20 Project 20 Done

---
+++ Project 20 - Ripple Effect On Button Click/js/app.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector(".ripple");
+
+btn.addEventListener("click", e => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const btnTop = e.target.offsetTop;
+  const btnLeft = e.target.offsetLeft;
+
+  const xInside = x - btnLeft;
+  const yInside = y - btnTop;
+
+  const circle = document.createElement("span");
+  circle.classList.add("circle");
+  circle.style.top = `${yInside}px`;
+  circle.style.left = `${xInside}px`;
+
+  btn.append(circle);
+  setTimeout(() => circle.remove(), 500);
+});
